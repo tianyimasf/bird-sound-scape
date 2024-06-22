c5b37f7e9f050228204f0f9d652838f83cc4f618
tianyimasf <inipop.tianyi@gmail.com>
2024-06-22 17:10:46 -0600

    more work

--- ios/ios/ContentView.swift
+++ ios/ios/ContentView.swift
@@ -6,34 +6,34 @@ struct ContentView: View {
     @State private var icon_info = getRandomizedIcon()
     
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 550)
-
-            CircleImage(icon_info: self.$icon_info)
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("John Doe")
-                    .font(.title)
-                HStack {
-                    Text(self.icon_info[1].capitalized)
-                        .font(.subheadline)
-                    Spacer()
-                    Text("📍Boulder, CO")
-                        .font(.subheadline)
+        ScrollView {
+            VStack {
+                MapView()
+                    .frame(height: 550)
+                
+                CircleImage(icon_info: self.$icon_info)
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                
+                VStack(alignment: .leading) {
+                    Text("John Doe")
+                        .font(.title)
+                    HStack {
+                        Text(self.icon_info[1].capitalized)
+                            .font(.subheadline)
+                        Spacer()
+                        Text("📍Boulder, CO")
+                            .font(.subheadline)
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                    
+                    Divider()
+                    
+                    Text(self.icon_info[2])
                 }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-
-                Divider()
-
-                Text("About Your Featured Bird")
-                    .font(.title2)
-                Text("Some bird description.")
+                .padding()
             }
-            .padding()
         }
     }
 }
--- ios/ios/helper.swift
+++ ios/ios/helper.swift
@@ -22,6 +22,15 @@ func getAllImageFiles() -> [String] {
     return imageFiles
 }
 
+struct Post : Codable {
+
+    var PostTitle: String
+
+    private enum CodingKeys : String, CodingKey {
+        case PostTitle = "Post Title"
+    }
+}
+
 func getRandomizedIcon() -> [String] {
     
     let imageFiles = getAllImageFiles()
@@ -31,7 +40,25 @@ func getRandomizedIcon() -> [String] {
     let chosenImageIndex = Int(arc4random_uniform(UInt32(numOfImages)))
     
     let filename = imageFiles[chosenImageIndex]
-    let species_name = filename.split(separator: "-")[0]
+    let species_name = String(filename.split(separator: "-")[0])
     
-    return [filename, String(species_name)]
+    if let filepath = Bundle.main.path(forResource: species_name, ofType: "txt") {
+        guard let file = freopen(filepath, "r", stdin) else {
+            return [filename, String(species_name), ""]
+        }
+        defer {
+            fclose(file)
+        }
+        
+        let desc = readLine()!
+        return [filename, String(species_name), desc]
+    } else {
+        print("File not found")
+        return [filename, String(species_name), ""]
+    }
+}
+
+func readJSONFile<T: Decodable>(with url: URL) throws -> T {
+    let data = try Data(contentsOf: url)
+    return try JSONDecoder().decode(T.self, from: data)
 }
